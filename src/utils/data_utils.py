a997e9adc110c51d142870d4aaee69c5ef60ef67
szakadan <daniel.sokach@gmail.com>
2025-07-19 19:16:32 +0200

    rename files and utils logic added to the data_utils file

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -25,3 +25,40 @@ def download_source_data_by_year(url: str, year: int, output_folder: str = "data
     else: 
         print(f"Exception: {response.status_code} - {response.reason}")
     return pd.read_csv(output_file)  
+
+
+def drop_hight_na_columns(df: pd.DataFrame, threshold: int = 0.6):
+    """
+        Removes columns where missing values are evaluated by the appropriate threshold.
+
+        Parameters:
+        df (pd.DataFrame): The input data frame
+        threshold (float): The proportion of missing values above which to delete (e.g. 0.7 = 70%)
+
+        Returns:
+        pd.DataFrame: The new data frame, without the problematic columns
+    """
+    na_ratio = df.isna().mean()
+    columns_to_drop = na_ratio[na_ratio > threshold].index
+    print(f"[INFO] Droping features {columns_to_drop}")
+    return df.drop(columns=columns_to_drop)
+
+
+def fill_na_median(df: pd.DataFrame, threshold: int = 0.3):
+    """
+        Fill the numerical columns with there median if the NA ration reaches the threshold
+        Parameters: 
+        df (pd.DataFrame): The input data frame
+        threshold (float): The proportion of missing values (e.g. 0.4 = 40%)
+
+        Returns:
+        pd.DataFrame: The new data frame, with the filled columns
+    """
+    numerical_df = df.select_dtypes(include="number")
+
+    na_ratio = numerical_df.isna().mean()
+    columns_to_drop = na_ratio[na_ratio < threshold].index
+    print(f"[INFO] Columns which will be filled with there median: {columns_to_drop}")
+    df[columns_to_drop] = df[columns_to_drop].fillna(df[columns_to_drop].median())
+    return df
+    
