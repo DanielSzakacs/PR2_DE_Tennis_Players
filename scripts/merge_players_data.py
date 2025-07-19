f55f5ece61ec3ebd9a707c81381f06284ce92d4a
szakadan <daniel.sokach@gmail.com>
2025-07-19 13:31:42 +0200

    dataframe merge logic updated

--- scripts/merge_players_data.py
+++ scripts/merge_players_data.py
@@ -2,45 +2,24 @@ import pandas as pd
 from tqdm import tqdm
 tqdm.pandas(desc="Merge players data with there match odds")
 
-def _normalize_names(name: str):
-    """
-    Normalize player names by removing dots, hyphens, apostrophes,
-    and converting to lowercase.
-    
-    Parameters:
-        name (str): Short format player name (e.g. "Popyrin A.")
-    
-    Returns:
-        str: Normalized player name (e.g. "popyrin a")
-    """
+def form_odds(name): 
     if pd.isnull(name):
-        return ""
-    name = name.replace(".", "").replace("-", "").replace("'", "")
-    return name.lower()
-
-def _normalize_full_name(name: str):
-    """
-    Normalize full player names by converting to a comparable short format:
-    [last name] [first initial], in lowercase.
-
-    Parameters:
-        name (str): Full player name (e.g. "Alexei Popyrin")
-
-    Returns:
-        str: Normalized name (e.g. "popyrin a")
-    """
-    if pd.isnull(name):
-        return ""
+        return None
     parts = name.replace(".", "").replace("-", "").replace("'", "").split()
-    if len(parts) < 2:
-        return name.lower()
-    last = parts[-1]
-    first_init = parts[0][0] if len(parts[0]) > 0 else ""
-    if len(parts) == 3:
-        middle = parts[1]
-        return f"{middle} {last} {first_init}".lower()
-    return f"{last} {first_init}".lower()
+    if(len(parts) > 2):
+        return None
     
+    return f"{parts[0][:3].lower()} {parts[1].lower()}"
+
+def form_atp(name): 
+    if pd.isnull(name):
+        return None
+    parts = name.replace(".", "").replace("-", "").replace("'", "").split()
+    if(len(parts) > 2):
+        return None
+    
+    return f"{parts[1][:3].lower()} {parts[0][0].lower()}"
+
 def merge_on_name_and_date(df1: pd.DataFrame, df2: pd.DataFrame, date_col: str = "date"):
     """
     Merge two tennis match datasets based on normalized player names and match dates.
@@ -61,14 +40,14 @@ def merge_on_name_and_date(df1: pd.DataFrame, df2: pd.DataFrame, date_col: str =
     df2.columns = df2.columns.str.lower()
 
     # Normalize the keys in the df1 (players games stas)
-    df1["winner_key"] = df1["winner"].apply(_normalize_full_name)
-    df1["loser_key"] = df1["loser"].apply(_normalize_full_name)
-    df1[date_col] = pd.to_datetime(df1[date_col])
+    df1["winner_key"] = df1["winner"].apply(form_atp)
+    df1["loser_key"] = df1["loser"].apply(form_atp)
+    df1[date_col] = pd.to_datetime(df1[date_col], format="%Y%m%d")
 
     # Normalize the keys in the df2 (odds)
-    df2["winner_key"] = df2["winner"].apply(_normalize_names)
-    df2["loser_key"] = df2["loser"].apply(_normalize_names)
-    df2[date_col] = pd.to_datetime(df2[date_col])
+    df2["winner_key"] = df2["winner"].apply(form_odds)
+    df2["loser_key"] = df2["loser"].apply(form_odds)
+    df2[date_col] = pd.to_datetime(df2[date_col], format="%Y-%m-%d")
 
     match_key_dict = {
         (row[date_col], frozenset([row["winner_key"], row["loser_key"]])): row["winner_key"]
@@ -92,7 +71,7 @@ def merge_on_name_and_date(df1: pd.DataFrame, df2: pd.DataFrame, date_col: str =
         df2,
         on=[date_col, "winner_key", "loser_key"],
         suffixes=("_df1", "_df2"),
-        how="left"
+        how="inner",
     )
     return merged
 
@@ -112,5 +91,11 @@ def merge_on_name_and_date(df1: pd.DataFrame, df2: pd.DataFrame, date_col: str =
 #     'loser': ['Thompson J.', 'Xy', 'Duckworth J.']
 # })
 
+# atp_df = pd.read_csv("./data/raw/atp_men_2000_2024/all_from_2000_to_2024.csv")
+# odds_df = pd.read_csv("./data/raw/atp_odds_2001_2024/all_from_2001_to_2024.csv")
+# atp_df = atp_df.rename(columns={"tourney_date": "date", "winner_name": "winner", "loser_name": "loser"})
+# odds_df = odds_df.rename(columns={"Date": "date", "Winner": "winner", "Loser": "loser"})
+
+# This test code runs all the time
 # merge = merge_on_name_and_date(atp_df, odds_df)
 # print(merge)
