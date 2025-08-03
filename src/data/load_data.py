1b8d62e21265451cce60b2c7184083616ffabd70
szakadan <daniel.sokach@gmail.com>
2025-08-03 16:08:10 +0200

    code updated

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -7,6 +7,6 @@ DEFAULT_INTERIM_DATA_PATH = os.getenv("DEFAULT_INTERIM_DATA_PATH")
 
 def load_interim_data(url: str = DEFAULT_INTERIM_DATA_PATH):
     if os.path.isfile(url):
-        return pd.read_csv(url, parse_dates=["date"], low_memory=False).sort_values(by="date").reset_index(drop=True)
+        return pd.read_csv(url, parse_dates=["tourney_date"], low_memory=False).sort_values(by="tourney_date").reset_index(drop=True)
     else: 
         print("[ERROR] Interim file do not exist")
