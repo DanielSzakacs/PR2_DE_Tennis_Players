13421177d3b62f0977c8398822d7bb98cd4701e0
szakadan <daniel.sokach@gmail.com>
2025-08-05 22:37:04 +0200

    new data loader function created

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -4,8 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DEFAULT_ATP_INTERIM_DATA_PATH = os.getenv("DEFAULT_ATP_INTERIM_DATA_PATH")
+DEFAULT_ATP_ENGINEERED_DATA_PATH = os.getenv("DEFAULT_ATP_ENGINEERED_DATA_PATH")
 
 def load_interim_data(url: str = DEFAULT_ATP_INTERIM_DATA_PATH):
+    if os.path.isfile(url):
+        return pd.read_csv(url, parse_dates=["tourney_date"], low_memory=False).sort_values(by="tourney_date").reset_index(drop=True)
+    else: 
+        print("[ERROR] Interim file do not exist")
+
+
+def load_features_data(url: str = DEFAULT_ATP_INTERIM_DATA_PATH):
     if os.path.isfile(url):
         return pd.read_csv(url, parse_dates=["tourney_date"], low_memory=False).sort_values(by="tourney_date").reset_index(drop=True)
     else: 
